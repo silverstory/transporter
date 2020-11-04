0741af3b1b8e5062d05c474db65c3fed2fc65a83
Eprel Acer ICTO <apr.medina@gmail.com>
2020-11-04 13:51:25 +0800

    Second commit

---
+++ aip-app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const customRouter = function (req) {
+  return 'http://192.168.23.60'; // protocol + host
+};
+
+const options = {
+  target: 'http://localhost:80',
+  router: customRouter,
+};
+
+const myProxy = createProxyMiddleware(options);
+
+const app = express();
+app.use(myProxy); // add the proxy to express
+
+app.listen(80);

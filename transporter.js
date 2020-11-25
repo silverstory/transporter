0d65d087e7a81ed9984a0660cd84bc35697c82cb
Eprel Acer ICTO <apr.medina@gmail.com>
2020-11-25 10:55:58 +0800

    Added transporter js

---
+++ transporter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const {
+  createProxyMiddleware
+} = require('http-proxy-middleware');
+
+const customRouter = function (req) {
+  return 'http://192.168.23.8:3000'; // protocol + host
+};
+
+const options = {
+  target: 'http://localhost:80',
+  router: customRouter,
+};
+
+const myProxy = createProxyMiddleware(options);
+
+const app = express();
+app.use(myProxy); // add the proxy to express
+
+app.listen(80);
